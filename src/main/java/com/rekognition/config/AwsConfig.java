4816afdb70ba2f72ad9b33dd04e7d18661081c83
Batuhan Küçükali <mail@batuhankucukali.com>
2018-04-17 22:09:36 +0300

    removed component annotation.

--- src/main/java/com/rekognition/config/AwsConfig.java
+++ src/main/java/com/rekognition/config/AwsConfig.java
@@ -3,11 +3,12 @@ package com.rekognition.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "aws")
 public class AwsConfig {
     private String apiKey;
